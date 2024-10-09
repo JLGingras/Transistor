013b86b783d1d192e0c28049598a4df86ac954eb
Joffrey-Lou Gingras <gingras.joffreylou.professionnel@gmail.com>
2024-10-09 19:56:40 -0400

    Ajoutez des fichiers projet.

---
+++ FactureTransistor/Factures/ListeDesFactures.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactureTransistor.Factures
+{
+    public partial class ListeDesFactures : Form
+    {
+        public ListeDesFactures()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FactureTransistor/Form.cs
@@ -0,0 +1,10 @@
+namespace FactureTransistor
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
