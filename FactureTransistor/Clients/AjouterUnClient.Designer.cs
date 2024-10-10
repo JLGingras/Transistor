194aecce4459f9324f00985a0a564f2bbe260e29
Joffrey-Lou Gingras <gingras.joffreylou.professionnel@gmail.com>
2024-10-09 20:20:08 -0400

    Complétion du formulaire d'ajout d'un client.

--- FactureTransistor/Clients/AjouterUnClient.Designer.cs
+++ FactureTransistor/Clients/AjouterUnClient.Designer.cs
@@ -28,12 +28,337 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AjouterUnClient";
+            NomGroupBox = new GroupBox();
+            pronomsTextBox = new TextBox();
+            pronomsLabel = new Label();
+            prenomTextBox = new TextBox();
+            prenomLabel = new Label();
+            nomTextBox = new TextBox();
+            nomLabel = new Label();
+            adresseGroupBox = new GroupBox();
+            codePostaleTextBox = new TextBox();
+            codePostaleLabel = new Label();
+            villeTextBox = new TextBox();
+            villeLabel = new Label();
+            rueTextBox = new TextBox();
+            rueLabel = new Label();
+            paysTextBox = new TextBox();
+            paysLabel = new Label();
+            provinceTextBox = new TextBox();
+            provinceLabel = new Label();
+            regionTextBox = new TextBox();
+            regionLabel = new Label();
+            enregistrerClient = new Button();
+            boutonAnnuler = new Button();
+            contactGroupBox = new GroupBox();
+            adresseEmailTexbox = new TextBox();
+            telephoneLabel = new Label();
+            telephonetextBox = new TextBox();
+            adresseEmailLabel = new Label();
+            NomGroupBox.SuspendLayout();
+            adresseGroupBox.SuspendLayout();
+            contactGroupBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // NomGroupBox
+            // 
+            NomGroupBox.Controls.Add(nomTextBox);
+            NomGroupBox.Controls.Add(nomLabel);
+            NomGroupBox.Controls.Add(prenomTextBox);
+            NomGroupBox.Controls.Add(prenomLabel);
+            NomGroupBox.Controls.Add(pronomsTextBox);
+            NomGroupBox.Controls.Add(pronomsLabel);
+            NomGroupBox.Location = new Point(12, 12);
+            NomGroupBox.Name = "NomGroupBox";
+            NomGroupBox.Size = new Size(356, 118);
+            NomGroupBox.TabIndex = 0;
+            NomGroupBox.TabStop = false;
+            NomGroupBox.Text = "Nom";
+            // 
+            // pronomsTextBox
+            // 
+            pronomsTextBox.Location = new Point(97, 25);
+            pronomsTextBox.Name = "pronomsTextBox";
+            pronomsTextBox.Size = new Size(251, 23);
+            pronomsTextBox.TabIndex = 1;
+            // 
+            // pronomsLabel
+            // 
+            pronomsLabel.AutoSize = true;
+            pronomsLabel.Location = new Point(6, 28);
+            pronomsLabel.Name = "pronomsLabel";
+            pronomsLabel.Size = new Size(63, 15);
+            pronomsLabel.TabIndex = 0;
+            pronomsLabel.Text = "Pronom(s)";
+            // 
+            // prenomTextBox
+            // 
+            prenomTextBox.Location = new Point(97, 57);
+            prenomTextBox.Name = "prenomTextBox";
+            prenomTextBox.Size = new Size(251, 23);
+            prenomTextBox.TabIndex = 3;
+            // 
+            // prenomLabel
+            // 
+            prenomLabel.AutoSize = true;
+            prenomLabel.Location = new Point(6, 57);
+            prenomLabel.Name = "prenomLabel";
+            prenomLabel.Size = new Size(49, 15);
+            prenomLabel.TabIndex = 2;
+            prenomLabel.Text = "Prénom";
+            // 
+            // nomTextBox
+            // 
+            nomTextBox.Location = new Point(97, 86);
+            nomTextBox.Name = "nomTextBox";
+            nomTextBox.Size = new Size(251, 23);
+            nomTextBox.TabIndex = 5;
+            // 
+            // nomLabel
+            // 
+            nomLabel.AutoSize = true;
+            nomLabel.Location = new Point(6, 89);
+            nomLabel.Name = "nomLabel";
+            nomLabel.Size = new Size(34, 15);
+            nomLabel.TabIndex = 4;
+            nomLabel.Text = "Nom";
+            // 
+            // adresseGroupBox
+            // 
+            adresseGroupBox.Controls.Add(paysTextBox);
+            adresseGroupBox.Controls.Add(paysLabel);
+            adresseGroupBox.Controls.Add(provinceTextBox);
+            adresseGroupBox.Controls.Add(provinceLabel);
+            adresseGroupBox.Controls.Add(regionTextBox);
+            adresseGroupBox.Controls.Add(regionLabel);
+            adresseGroupBox.Controls.Add(codePostaleTextBox);
+            adresseGroupBox.Controls.Add(codePostaleLabel);
+            adresseGroupBox.Controls.Add(villeTextBox);
+            adresseGroupBox.Controls.Add(villeLabel);
+            adresseGroupBox.Controls.Add(rueTextBox);
+            adresseGroupBox.Controls.Add(rueLabel);
+            adresseGroupBox.Location = new Point(12, 136);
+            adresseGroupBox.Name = "adresseGroupBox";
+            adresseGroupBox.Size = new Size(356, 206);
+            adresseGroupBox.TabIndex = 6;
+            adresseGroupBox.TabStop = false;
+            adresseGroupBox.Text = "Adresse";
+            // 
+            // codePostaleTextBox
+            // 
+            codePostaleTextBox.Location = new Point(97, 86);
+            codePostaleTextBox.Name = "codePostaleTextBox";
+            codePostaleTextBox.Size = new Size(251, 23);
+            codePostaleTextBox.TabIndex = 5;
+            // 
+            // codePostaleLabel
+            // 
+            codePostaleLabel.AutoSize = true;
+            codePostaleLabel.Location = new Point(6, 89);
+            codePostaleLabel.Name = "codePostaleLabel";
+            codePostaleLabel.Size = new Size(76, 15);
+            codePostaleLabel.TabIndex = 4;
+            codePostaleLabel.Text = "Code Postale";
+            // 
+            // villeTextBox
+            // 
+            villeTextBox.Location = new Point(97, 57);
+            villeTextBox.Name = "villeTextBox";
+            villeTextBox.Size = new Size(251, 23);
+            villeTextBox.TabIndex = 3;
+            // 
+            // villeLabel
+            // 
+            villeLabel.AutoSize = true;
+            villeLabel.Location = new Point(6, 57);
+            villeLabel.Name = "villeLabel";
+            villeLabel.Size = new Size(29, 15);
+            villeLabel.TabIndex = 2;
+            villeLabel.Text = "Ville";
+            // 
+            // rueTextBox
+            // 
+            rueTextBox.Location = new Point(97, 25);
+            rueTextBox.Name = "rueTextBox";
+            rueTextBox.Size = new Size(251, 23);
+            rueTextBox.TabIndex = 1;
+            // 
+            // rueLabel
+            // 
+            rueLabel.AutoSize = true;
+            rueLabel.Location = new Point(6, 28);
+            rueLabel.Name = "rueLabel";
+            rueLabel.Size = new Size(27, 15);
+            rueLabel.TabIndex = 0;
+            rueLabel.Text = "Rue";
+            // 
+            // paysTextBox
+            // 
+            paysTextBox.Location = new Point(97, 176);
+            paysTextBox.Name = "paysTextBox";
+            paysTextBox.ReadOnly = true;
+            paysTextBox.Size = new Size(251, 23);
+            paysTextBox.TabIndex = 11;
+            paysTextBox.Text = "Canada";
+            // 
+            // paysLabel
+            // 
+            paysLabel.AutoSize = true;
+            paysLabel.Location = new Point(6, 179);
+            paysLabel.Name = "paysLabel";
+            paysLabel.Size = new Size(31, 15);
+            paysLabel.TabIndex = 10;
+            paysLabel.Text = "Pays";
+            // 
+            // provinceTextBox
+            // 
+            provinceTextBox.Location = new Point(97, 147);
+            provinceTextBox.Name = "provinceTextBox";
+            provinceTextBox.ReadOnly = true;
+            provinceTextBox.Size = new Size(251, 23);
+            provinceTextBox.TabIndex = 9;
+            provinceTextBox.Text = "Québec";
+            // 
+            // provinceLabel
+            // 
+            provinceLabel.AutoSize = true;
+            provinceLabel.Location = new Point(6, 150);
+            provinceLabel.Name = "provinceLabel";
+            provinceLabel.RightToLeft = RightToLeft.Yes;
+            provinceLabel.Size = new Size(53, 15);
+            provinceLabel.TabIndex = 8;
+            provinceLabel.Text = "Province";
+            // 
+            // regionTextBox
+            // 
+            regionTextBox.Location = new Point(97, 118);
+            regionTextBox.Name = "regionTextBox";
+            regionTextBox.ReadOnly = true;
+            regionTextBox.Size = new Size(251, 23);
+            regionTextBox.TabIndex = 7;
+            regionTextBox.Text = "Bas-Saint-Laurent";
+            // 
+            // regionLabel
+            // 
+            regionLabel.AutoSize = true;
+            regionLabel.Location = new Point(6, 121);
+            regionLabel.Name = "regionLabel";
+            regionLabel.Size = new Size(44, 15);
+            regionLabel.TabIndex = 6;
+            regionLabel.Text = "Région";
+            // 
+            // enregistrerClient
+            // 
+            enregistrerClient.Location = new Point(142, 441);
+            enregistrerClient.Name = "enregistrerClient";
+            enregistrerClient.Size = new Size(109, 23);
+            enregistrerClient.TabIndex = 7;
+            enregistrerClient.Text = "Enregistrer";
+            enregistrerClient.UseVisualStyleBackColor = true;
+            // 
+            // boutonAnnuler
+            // 
+            boutonAnnuler.Location = new Point(258, 441);
+            boutonAnnuler.Name = "boutonAnnuler";
+            boutonAnnuler.Size = new Size(110, 23);
+            boutonAnnuler.TabIndex = 8;
+            boutonAnnuler.Text = "Annuler";
+            boutonAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // contactGroupBox
+            // 
+            contactGroupBox.Controls.Add(adresseEmailTexbox);
+            contactGroupBox.Controls.Add(telephoneLabel);
+            contactGroupBox.Controls.Add(telephonetextBox);
+            contactGroupBox.Controls.Add(adresseEmailLabel);
+            contactGroupBox.Location = new Point(12, 348);
+            contactGroupBox.Name = "contactGroupBox";
+            contactGroupBox.Size = new Size(356, 87);
+            contactGroupBox.TabIndex = 6;
+            contactGroupBox.TabStop = false;
+            contactGroupBox.Text = "Contact";
+            // 
+            // adresseEmailTexbox
+            // 
+            adresseEmailTexbox.Location = new Point(97, 57);
+            adresseEmailTexbox.Name = "adresseEmailTexbox";
+            adresseEmailTexbox.Size = new Size(251, 23);
+            adresseEmailTexbox.TabIndex = 3;
+            // 
+            // telephoneLabel
+            // 
+            telephoneLabel.AutoSize = true;
+            telephoneLabel.Location = new Point(6, 33);
+            telephoneLabel.Name = "telephoneLabel";
+            telephoneLabel.Size = new Size(61, 15);
+            telephoneLabel.TabIndex = 2;
+            telephoneLabel.Text = "Téléphone";
+            // 
+            // telephonetextBox
+            // 
+            telephonetextBox.Location = new Point(97, 25);
+            telephonetextBox.Name = "telephonetextBox";
+            telephonetextBox.Size = new Size(251, 23);
+            telephonetextBox.TabIndex = 1;
+            // 
+            // adresseEmailLabel
+            // 
+            adresseEmailLabel.AutoSize = true;
+            adresseEmailLabel.Location = new Point(7, 60);
+            adresseEmailLabel.Name = "adresseEmailLabel";
+            adresseEmailLabel.Size = new Size(36, 15);
+            adresseEmailLabel.TabIndex = 0;
+            adresseEmailLabel.Text = "Email";
+            // 
+            // AjouterUnClient
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(379, 469);
+            Controls.Add(contactGroupBox);
+            Controls.Add(boutonAnnuler);
+            Controls.Add(enregistrerClient);
+            Controls.Add(adresseGroupBox);
+            Controls.Add(NomGroupBox);
+            Name = "AjouterUnClient";
+            Text = "Ajouter Un Client";
+            NomGroupBox.ResumeLayout(false);
+            NomGroupBox.PerformLayout();
+            adresseGroupBox.ResumeLayout(false);
+            adresseGroupBox.PerformLayout();
+            contactGroupBox.ResumeLayout(false);
+            contactGroupBox.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox NomGroupBox;
+        private TextBox pronomsTextBox;
+        private Label pronomsLabel;
+        private TextBox nomTextBox;
+        private Label nomLabel;
+        private TextBox prenomTextBox;
+        private Label prenomLabel;
+        private GroupBox adresseGroupBox;
+        private TextBox codePostaleTextBox;
+        private Label codePostaleLabel;
+        private TextBox villeTextBox;
+        private Label villeLabel;
+        private TextBox rueTextBox;
+        private Label rueLabel;
+        private TextBox paysTextBox;
+        private Label paysLabel;
+        private TextBox provinceTextBox;
+        private Label provinceLabel;
+        private TextBox regionTextBox;
+        private Label regionLabel;
+        private Button enregistrerClient;
+        private Button boutonAnnuler;
+        private GroupBox contactGroupBox;
+        private TextBox adresseEmailTexbox;
+        private Label telephoneLabel;
+        private TextBox telephonetextBox;
+        private Label adresseEmailLabel;
     }
 }
